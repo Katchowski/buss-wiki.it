b90de3afbd573750892df5578eca5a24224838d2
katchowski <jt.jkaowski@gmail.com>
2024-06-04 14:33:33 -0500

    isolating issues in lua

--- main.lua
+++ main.lua
@@ -4,20 +4,20 @@ local list = get("list")
 loadSites.on_click(showList())
 
 function getSites()
-    local res = fetch({
-        url = "https://api.buss.lol/domains",
-        method = "GET"
-    })
-    return res
+    --local res = fetch({
+        --url = "https://api.buss.lol/domains",
+        --method = "GET"
+    --})
+    return "returned value"
 end
 
 function showList()
     loadSites.set_content("Loading...")
-    local response = getSites()
-    local data = response:json()
-    createList(data)
+    --local response = getSites()
+    --local data = response:json()
+    --createList(data)
 end
 
 function createList(data)
-    list.set_content(data)
+    list.set_content("It is working!")
 end
