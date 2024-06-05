41ad246bbb98a6a35dad721faafc9247f76540d5
katchowski <jt.jkaowski@gmail.com>
2024-06-04 20:26:05 -0500

    e

--- script.lua
+++ script.lua
@@ -1,25 +1,30 @@
 print('Hello, World!')
 
-local loadSites = get('loadSites')
+--local loadSites = get('loadSites')
 local list = get('list')
 
-loadSites.on_click(ShowList())
+--loadSites.set_content('Loading...')
+list.set_content('It is working!')
+
+print('Hello Again, World!')
+
+--[[loadSites.on_click(ShowList())
 
 function GetSites()
-    --local res = fetch({
-        --url = "https://api.buss.lol/domains",
-        --method = "GET"
-    --})
+    local res = fetch({
+        url = "https://api.buss.lol/domains",
+        method = "GET"
+    })
     return "returned value"
 end
 
 function ShowList()
     loadSites.set_content('Loading...')
-    --local response = getSites()
-    --local data = response:json()
-    --createList(data)
+    local response = getSites()
+    local data = response:json()
+    createList(data)
 end
 
 function CreateList(data)
     list.set_content('It is working!')
-end
+end]]--
